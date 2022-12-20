a5d4993a86415c5ce0e668cdcc1b89a94f8050a2
hoanguyen <vanhoaltw@gmail.com>
2022-12-20 19:20:12 +0700

    remove some package

--- dist/generated/graphql.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CacheControlScope = void 0;
-var CacheControlScope;
-(function (CacheControlScope) {
-    CacheControlScope["Private"] = "PRIVATE";
-    CacheControlScope["Public"] = "PUBLIC";
-})(CacheControlScope = exports.CacheControlScope || (exports.CacheControlScope = {}));
-//# sourceMappingURL=graphql.js.map
--- dist/settings/env/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const env = process.env.NODE_ENV || 'development';
-const envConfig = require(`./${env}`);
-exports.default = envConfig.default;
-//# sourceMappingURL=index.js.map
